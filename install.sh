59c2351f8d0c2cd7909e90df4be13b5441b7f6ec
pcbt <batutr2@gmail.com>
2019-03-08 19:40:08 +0300

    bug fixes

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get update
 sudo apt-get install ppp wiringpi screen minicom
-sudo cp /boot/config.txt ~/boot/confix.txt
-sudo cp /boot/cmdline.txt ~/boot/cmdline.txt
-sudo cp /peers/airnet ~/etc/ppp/peers/
+sudo cp ./boot/config.txt ~/boot/config.txt
+sudo cp ./boot/cmdline.txt ~/boot/cmdline.txt
+sudo cp ./peers/airnet ~/etc/ppp/peers/
